91289eed18045564d2048c6076c5e5dbc395911b
urmoonfriend <konyspay.aidos@gmail.com>
2024-07-21 21:14:18 +0500

    init

---
+++ src/main/java/kz/company/r_proxy_service/RProxyServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.company.r_proxy_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RProxyServiceApplication.class, args);
+	}
+
+}
